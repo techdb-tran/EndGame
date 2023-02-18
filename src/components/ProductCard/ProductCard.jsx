d2e70f8820b5c4e21bb3e9d1d443d11dd41bac26
sangtruong015 <sangtruongqna@gmail.com>
2023-02-18 19:53:44 +0700

    sang_2

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
+import prodcompare from "../../assets/images/prodcompare.svg";
+import wish from "../../assets/images/wish.svg";
+// import wishlist from "../../assets/images/wishlist.svg";
+import watch from "../../assets/images/watch.jpg";
+import watch2 from "../../assets/images/watch.jpg";
+import addcart from "../../assets/images/add-cart.svg";
+import view from "../../assets/images/view.svg";
+
+const ProductCard = () => {
+  return (
+    <>
+      <div className="col-3">
+        <Link to="/" className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <button className="border-0 bg-transparent">
+              <img src={wish} alt="wishlist" />
+            </button>
+          </div>
+          <div className="product-image">
+            <img src={watch} className="img-fluid" alt="product-img" />
+            <img src={watch2} className="img-fluid" alt="product-img" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">
+              Kids headphones bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className="price">$100.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <button className="border-0 bg-transparent">
+                <img src={prodcompare} alt="compare" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={view} alt="view" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={addcart} alt="addcart" />
+              </button>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div className="col-3">
+        <Link to="/" className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <button className="border-0 bg-transparent">
+              <img src={wish} alt="wishlist" />
+            </button>
+          </div>
+          <div className="product-image">
+            <img src={watch} className="img-fluid" alt="product-img" />
+            <img src={watch2} className="img-fluid" alt="product-img" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">
+              Kids headphones bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className="price">$100.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <button className="border-0 bg-transparent">
+                <img src={prodcompare} alt="compare" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={view} alt="view" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={addcart} alt="addcart" />
+              </button>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ProductCard;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BlogCard from "../../components/BlogCard/BlogCard";
+import ProductCard from "../../components/ProductCard/ProductCard";
+
+const Home = () => {
+  return (
+    <>
+      <BlogCard />
+      <ProductCard />
+    </>
+  );
+};
+
+export default Home;
