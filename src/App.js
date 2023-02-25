8a1afe1d1dc96f6e0cd745d6025694464d3ab3f0
sangtruong015 <sangtruongqna@gmail.com>
2023-02-25 18:14:07 +0700

    sang_4

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ import Order from "./components/AdminPage/Order";
 import DashBoard from "./components/AdminPage/DashBoard";
 import OurStore from "./pages/OurStore/OurStore";
 import UserManagement from "./components/AdminPage/UserManagement";
+import SingleProduct from "./pages/SingleProduct/SingleProduct";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/SignUp/SignUp";
+import Wishlist from "./pages/Wishlist/Wishlist";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+import SingleBlog from "./pages/SingleBlog/SingleBlog";
+import Checkout from "./pages/Checkout/Checkout";
 
 function App() {
   return (
@@ -19,9 +26,16 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="/product" element={<OurStore />} />
-            <Route path="/blogs" element={<Blog />} />
+            <Route path="product" element={<OurStore />} />
+            <Route path="blogs" element={<Blog />} />
             <Route path="cart" element={<Cart />} />
+            <Route path="product/:id" element={<SingleProduct />} />
+            <Route path="blog/:id" element={<SingleBlog />} />
+            <Route path="wishlist" element={<Wishlist />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="forgot-password" element={<ForgotPassword />} />
+            <Route path="checkout" element={<Checkout />} />
           </Route>
           <Route path="/admin" element={<AdminLayout />}>
             <Route path="dashboard" element={<DashBoard />} />
